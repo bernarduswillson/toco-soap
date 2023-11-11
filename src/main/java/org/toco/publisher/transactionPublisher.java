07aa697c6d4b120d048b115d46f985b3ae125bb6
Raditss <raditya.naufal.a@gmail.com>
2023-11-11 10:58:08 +0700

    fix dedpendencies and build

--- src/main/java/org/toco/publisher/transactionPublisher.java
+++ src/main/java/org/toco/publisher/transactionPublisher.java
@@ -3,7 +3,13 @@ import javax.xml.ws.Endpoint;
 
 public class transactionPublisher {
     public static void publish() {
+        System.out.println("publishing transaction service");
         Endpoint.publish("http://localhost:8080/transaction", new org.toco.service.transaction());
+        System.out.println("transaction service published");
     }
 
 }
+
+
+
+
